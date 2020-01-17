41c650c50811a5660313a12cd79b75659ab11dc3
Benjamin Thom <jaminthom@gmail.com>
2020-01-17 10:01:09 -0800

    Update scripts.js and index.html with example code to test for usability. Research programming languages and questions to ask users.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Front-end
+
+.btn {
+  display: block;
+}
+
+#quote {
+  display: none;
+}
+
+// Back-end
+
+$(document).ready(function() {
+  $("form#insurance").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    var gender = $("select#gender").val();
+
+    if (age) {
+      var quote = (100 - age) * 3;
+      if (gender === 'male' && age < 26) {
+        quote += 50;
+      }
+
+      $("#rate").empty().append(quote);
+      $("#quote").show();
+    } else {
+      alert('Please enter your age.');
+    }
+
+    event.preventDefault();
+  });
+});
