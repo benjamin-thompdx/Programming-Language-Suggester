7a347e8b090d5062840297dc3b79be41f610f53f
Benjamin Thom <jaminthom@gmail.com>
2020-01-17 12:58:43 -0800

    Update scripts.js for if, else if, and else statements

--- js/scripts.js
+++ js/scripts.js
@@ -12,21 +12,21 @@
 
 $(document).ready(function() {
   $("form#language-suggester").submit(function(event) {
-    var skills = $("#skill-level").val();
+    var skills = ($("#skill-level").val();
     var results = $("input:radio[name=desired-results]:checked").val();
     var money = $("#money").val();
     var dominance = $("select#dominance").val();
     var monitor = $("select#monitor").val();
 
-    if (skills) {
-      var code = JavaScript;
-      if (gender === 'male' && age < 26) {
-        code = React;
-      }
-
+    if (skills === 'Intermediate, ') {
+      $("#suggestion") = JavaScript;
+    } else if (dominance === 'left') {
+      $("#suggestion")= React;
     } else {
-      code = Cow;
+      $("#suggestion") = Cow;
     }
+
+    $("#suggestion".show();
 
     event.preventDefault();
   });
