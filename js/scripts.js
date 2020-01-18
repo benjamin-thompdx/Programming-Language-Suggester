535ba4afce08ff678bd0b7940dfecca8cb0d0039
Benjamin Thom <jaminthom@gmail.com>
2020-01-17 16:38:24 -0800

    Update if, else if, and else statements, add two additonal answers for results question, add styiling to styles.css and update answer button to primary

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,43 @@ $(document).ready(function(){
     userMoney = $("input:radio[name=money]:checked").val();
     userDominance = $("input:radio[name=dominance]:checked").val();
     userMonitor = $("input:radio[name=monitor]:checked").val();
+
+
+    if (userMoney === "kinda" || userMoney === "sorta") {
+      $(".money-kinda-sorta").show();
+    } else if (userMoney === "no" || userMoney === "hmmm") {
+      $(".money-no-hmmm").show();
+      if (userSkill === "intermediate" && userResults === "basics" && userMonitor === "single") {
+        $(".javascript").show();
+      } else if (userSkill === "advanced" && userResults === "more" && userMonitor === "multiple") {
+        $(".react").show();
+      } else if (userSkill === "expert" && userResults === "new" && userMonitor  === "multiple") {
+        $(".cow").show();
+      } else {
+        $(".sql").show();
+      }
+    } else if (userMoney === "yes") {
+      $(".money-yes").show();
+      if (userSkill === "intermediate" && userResults === "basics" && userMonitor === "single") {
+        $(".javascript").show();
+      } else if (userSkill === "advanced" && userResults === "more" && userMonitor === "multiple") {
+        $(".react").show();
+      } else if (userSkill === "expert" && userResults === "new" && userMonitor  === "multiple") {
+        $(".cow").show();
+      } else {
+        $(".sql").show();
+      }
+    } else {
+      alert("Answer all questions to see your suggested language!")
+      $(".opening").show();
+      $("#language-suggester").show();
+    }
+
+    $("button.btn-primary").click(function(){
+      $(".result").hide();
+      $(".opening").show(); 
+      $("#language-suggester").show();
+    });
+    event.preventDefault();
+  });
+});
